b04ec7819020a4216939d6531f277aa8833088e3
heungbae <heungabe@com2us.com>
2025-03-26 15:02:26 +0900

    .NET9 로 마이그레이션 및 리팩토링 중

---
+++ SuperSocketLite/SocketAsyncEventArgsProxy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using SuperSocket.SocketBase;
+
+
+namespace SuperSocket.SocketEngine;
+
+class SocketAsyncEventArgsProxy
+{
+    public SocketAsyncEventArgs SocketEventArgs { get; private set; }
+
+    public int OrigOffset { get; private set; }
+
+    public bool IsRecyclable { get; private set; }
+
+    private SocketAsyncEventArgsProxy()
+    {
+
+    }
+
+    public SocketAsyncEventArgsProxy(SocketAsyncEventArgs socketEventArgs)
+        : this(socketEventArgs, true)
+    {
+        
+    }
+
+    public SocketAsyncEventArgsProxy(SocketAsyncEventArgs socketEventArgs, bool isRecyclable)
+    {
+        SocketEventArgs = socketEventArgs;
+        OrigOffset = socketEventArgs.Offset;
+        SocketEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(SocketEventArgs_Completed);
+        IsRecyclable = isRecyclable;
+    }
+
+    static void SocketEventArgs_Completed(object sender, SocketAsyncEventArgs e)
+    {
+        var socketSession = e.UserToken as IAsyncSocketSession;
+
+        if (socketSession == null)
+            return;
+
+        if (e.LastOperation == SocketAsyncOperation.Receive)
+        {
+            socketSession.AsyncRun(() => socketSession.ProcessReceive(e));
+        }
+        else
+        {
+            throw new ArgumentException("The last operation completed on the socket was not a receive");
+        }
+    } 
+
+    public void Initialize(IAsyncSocketSession socketSession)
+    {
+        SocketEventArgs.UserToken = socketSession;
+    }
+
+    public void Reset()
+    {
+        SocketEventArgs.UserToken = null;
+    }
+}
